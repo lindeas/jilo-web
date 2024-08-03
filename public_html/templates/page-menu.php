22e502c90c42706e43c152994d982d3b50064c4c
Yasen Pramatarov <yasen@lindeas.com>
2024-08-03 16:25:42 +0300

    Fixes sidebar

--- public_html/templates/page-menu.php
+++ public_html/templates/page-menu.php
@@ -2,6 +2,10 @@
         <!-- Menu -->
         <div class="menu-container">
             <ul class="menu-left">
+                <button class="btn btn-primary mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
+                    sidebar
+                </button>
+
                 <li><a href="index.php">home</a></li>
 <?php if ( isset($_SESSION['username']) ) { ?>
                 <li><a href="?page=config">config</a></li>
--- public_html/templates/page-sidebar.php
+++ public_html/templates/page-sidebar.php
@@ -1,18 +1,13 @@
         <div class="row">
 
             <!-- Sidebar -->
-            <div class="col-md-3">
-                <button class="btn btn-primary mb-3" type="button" data-toggle="collapse" data-target="#sidebar" aria-expanded="false" aria-controls="sidebar">
-                    sidebar
-                </button>
-                <div class="collapse show" id="sidebar">
-                    <div class="card card-body bg-light">
-                        <ul class="list-group">
-                            <li class="list-group-item"><a href="#">stats</a></li>
-                            <li class="list-group-item"><a href="#">config</a></li>
-                            <li class="list-group-item"><a href="#">logs</a></li>
-                        </ul>
-                    </div>
+            <div class="col-md-3 sidebar-wrapper collapse show" id="sidebar">
+                <div class="card card-body bg-light">
+                    <ul class="list-group">
+                        <li class="list-group-item"><a href="#">stats</a></li>
+                        <li class="list-group-item"><a href="#">config</a></li>
+                        <li class="list-group-item"><a href="#">logs</a></li>
+                    </ul>
                 </div>
             </div>
             <!-- /Sidebar -->
