a6cf2fe99b8dc1c914cc29d275ce8ca96f75e491
Yasen Pramatarov <yasen@lindeas.com>
2024-08-13 22:59:11 +0300

    Updates config file

--- app/config/jilo-web.conf.php
+++ app/config/jilo-web.conf.php
@@ -1,20 +1,34 @@
 <?php
 
 return [
+
+    //*******************
     // edit to customize
-    'domain'			=> 'localhost',			// domain for the web app
-    'folder'			=> '/jilo-web/',		// subfolder for the web app, if any
-    'jilo_database'		=> '../../jilo/jilo.db',	// database with logs from Jilo
+    //*******************
 
-    'registration_enabled'	=> true,			// set to false to disable new registrations
-    'login_message'		=> '',				// will be displayed on login screen
+    // domain for the web app
+    'domain'			=> 'localhost',
+    // subfolder for the web app, if any
+    'folder'			=> '/jilo-web/',
+    // database with logs from Jilo
+    'jilo_database'		=> '../../jilo/jilo.db',
+    // set to false to disable new registrations
+    'registration_enabled'	=> true,
+    // will be displayed on login screen
+    'login_message'		=> '',
 
+    //*******************************************
     // edit only if needed for tests or debugging
+    //*******************************************
+
     'db' => [
-        'db_type'		=> 'sqlite',			// DB type for the web app, currently only "sqlite" is used
-        'sqlite_file'		=> '../app/jilo-web.db',	// default is ../app/jilo-web.db
+        // DB type for the web app, currently only "sqlite" is used
+        'db_type'		=> 'sqlite',
+        // default is ../app/jilo-web.db
+        'sqlite_file'		=> '../app/jilo-web.db',
     ],
-    'version'			=> '0.1.1',			// system info
+    // system info
+    'version'			=> '0.1.1',
 ];
 
 ?>
