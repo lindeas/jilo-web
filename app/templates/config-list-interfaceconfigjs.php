0bab284e99af6fe98909c8879dadba41c1f52646
Yasen Pramatarov <yasen@lindeas.com>
2024-08-29 13:01:46 +0300

    Adds interface_config.js capability

--- app/templates/config-list-interfaceconfigjs.php
+++ app/templates/config-list-interfaceconfigjs.php
@@ -1,14 +1,15 @@
 
                 <!-- widget "config" -->
                 <div class="card text-center w-75 mx-lef">
-                    <p class="h4 card-header">Jilo web configuration</p>
+                    <p class="h4 card-header">Configuration of the Jitsi platform <strong><?= htmlspecialchars($platformDetails['name']) ?></strong></p>
                     <div class="card-body">
-                        <p class="card-text">platform variables</p>
+                        <p class="card-text">URL: <?= htmlspecialchars($platformDetails['jitsi_url']) ?></p>
+                        <p class="card-text">interface_config.js</p>
+<pre style="text-align: left;">
 <?php
-include '../app/helpers/render.php';
-renderConfig($config, '0');
-echo "\n";
+echo htmlspecialchars($platformInterfaceConfigjs);
 ?>
+</pre>
                     </div>
                 </div>
                 <!-- /widget "config" -->
