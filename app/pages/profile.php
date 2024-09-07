06a534c2daeadf991a1f5fbda60988d25ad0c383
Yasen Pramatarov <yasen@lindeas.com>
2024-09-08 01:36:57 +0300

    Adds profile edit page

--- app/pages/profile.php
+++ app/pages/profile.php
@@ -1,12 +1,20 @@
 <?php
 
+$action = $_REQUEST['action'] ?? '';
 require '../app/classes/user.php';
 
 $userObject = new User($dbWeb);
 
 $userDetails = $userObject->getUserDetails($user);
 
-include '../app/templates/profile.php';
+switch ($action) {
 
+    case 'edit':
+        include '../app/templates/profile-edit.php';
+        break;
+
+    default:
+        include '../app/templates/profile.php';
+}
 
 ?>
--- app/templates/profile-edit.php
+++ app/templates/profile-edit.php
@@ -6,62 +6,65 @@
                     <div class="card-body">
 
                         <div class="row">
+                            <p class="border rounded bg-light mb-4"><small>edit the profile fields</small></p>
 
                             <div class="col-md-4">
-                                <div class="border" style="width:200px; height: 200px;"><img src="" alt="avatar" /></div>
+                                <div class="border" style="width: 200px; height: 200px;"><img src="" alt="avatar" /></div>
                             </div>
 
                             <div class="col-md-8">
 
                                 <div class="row mb-3">
                                     <div class="col-md-4 text-end">
-                                        <label class="form-label"><small>username:</small></label>
+                                        <label for="username" class="form-label"><small>username:</small></label>
+                                        <span class="text-danger" style="margin-right: -12px;">*</span>
                                     </div>
                                     <div class="col-md-8 text-start bg-light">
-                                        <?= $userDetails[0]['username'] ?>
+                                        <input class="form-control" type="text" name="username" value="<?= $userDetails[0]['username'] ?>" required />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <div class="col-md-4 text-end">
-                                        <label class="form-label"><small>name:</small></label>
+                                        <label for="name" class="form-label"><small>name:</small></label>
                                     </div>
                                     <div class="col-md-8 text-start bg-light">
-                                        <?= $userDetails[0]['name'] ?>
+                                        <input class="form-control" type="text" name="name" value="<?= $userDetails[0]['name'] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <div class="col-md-4 text-end">
-                                        <label class="form-label"><small>email:</small></label>
+                                        <label for="email" class="form-label"><small>email:</small></label>
                                     </div>
                                     <div class="col-md-8 text-start bg-light">
-                                        <?= $userDetails[0]['email'] ?>
+                                        <input class="form-control" type="text" name="email" value="<?= $userDetails[0]['email'] ?>" />
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <div class="col-md-4 text-end">
-                                        <label class="form-label"><small>bio:</small></label>
+                                        <label for="bio" class="form-label"><small>bio:</small></label>
                                     </div>
                                     <div class="col-md-8 text-start bg-light">
-                                        <textarea class="scroll-box" rows="10"><?= $userDetails[0]['bio'] ?? '' ?></textarea>
+                                        <textarea class="form-control" name="bio" rows="10"><?= $userDetails[0]['bio'] ?? '' ?></textarea>
                                     </div>
                                 </div>
 
                                 <div class="row mb-3">
                                     <div class="col-md-4 text-end">
-                                        <label class="form-label"><small>rights:</small></label>
+                                        <label for="rights" class="form-label"><small>rights:</small></label>
                                     </div>
                                     <div class="col-md-8 text-start bg-light">
-                                        <?= $userDetails[0]['rights'] ?? '' ?>
+                                        <input class="form-control" type="text" name="rights" value="<?= $userDetails[0]['rights'] ?? '' ?>" />
                                     </div>
                                 </div>
 
                             </div>
 
                                 <p>
-                                    <a href="<?= $app_root ?>?page=profile&action=edit" class="btn btn-primary">Edit</a>
+                                    <a href="<?= $app_root ?>?page=profile" class="btn btn-primary">Cancel</a>
+                                    <a href="<?= $app_root ?>?page=profile&action=edit" class="btn btn-danger">Save</a>
                                 </p>
 
                         </div>
