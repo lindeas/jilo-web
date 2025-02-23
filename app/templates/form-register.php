ecb4e0fab42dfedba920e7d17364eabe6d783054
Yasen Pramatarov <yasen@lindeas.com>
2025-02-23 17:28:20 +0200

    Fixes login and register forms

--- app/templates/form-register.php
+++ app/templates/form-register.php
@@ -4,12 +4,22 @@
             <div class="card-body">
                 <p class="card-text">Enter credentials for registration:</p>
                 <form method="POST" action="<?= htmlspecialchars($app_root) ?>?page=register">
-                    <input type="text" name="username" placeholder="Username" required autofocus />
-                    <br />
-                    <input type="password" name="password" placeholder="Password" required />
-                    <br />
-                    <input type="password" name="confirm_password" placeholder="Confirm password" required />
-                    <br />&nbsp;<br />
+<?php include 'csrf_token.php'; ?>
+                    <div class="form-group mb-3">
+                        <input type="text" class="form-control w-50 mx-auto" name="username" placeholder="Username"
+                            pattern="[A-Za-z0-9_\-]{3,20}" title="3-20 characters, letters, numbers, - and _"
+                            required autofocus />
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="password" class="form-control w-50 mx-auto" name="password" placeholder="Password"
+                            pattern=".{8,}" title="Eight or more characters"
+                            required />
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type="password" class="form-control w-50 mx-auto" name="confirm_password" placeholder="Confirm password"
+                            pattern=".{8,}" title="Eight or more characters"
+                            required />
+                    </div>
                     <input type="submit" class="btn btn-primary" value="Register" />
                 </form>
             </div>
