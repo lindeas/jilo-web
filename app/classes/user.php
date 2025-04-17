40c646291ea5c74079707b68cd78ccd0506e0333
Yasen Pramatarov <yasen@lindeas.com>
2025-04-17 10:45:29 +0300

    Removes old registration core code

--- app/classes/user.php
+++ app/classes/user.php
@@ -34,63 +34,6 @@ class User {
 
 
     /**
-     * Registers a new user.
-     *
-     * @param string $username The username of the new user.
-     * @param string $password The password for the new user.
-     *
-     * @return bool|string True if registration is successful, error message otherwise.
-     */
-    public function register($username, $password) {
-        try {
-            // we have two inserts, start a transaction
-            $this->db->beginTransaction();
-
-            // hash the password, don't store it plain
-            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
-            // insert into users table
-            $sql = 'INSERT
-                        INTO users (username, password)
-                        VALUES (:username, :password)';
-            $query = $this->db->prepare($sql);
-            $query->bindValue(':username', $username);
-            $query->bindValue(':password', $hashedPassword);
-
-            // execute the first query
-            if (!$query->execute()) {
-                // rollback on error
-                $this->db->rollBack();
-                return false;
-            }
-
-            // insert the last user id into users_meta table
-            $sql2 = 'INSERT
-                        INTO users_meta (user_id)
-                        VALUES (:user_id)';
-            $query2 = $this->db->prepare($sql2);
-            $query2->bindValue(':user_id', $this->db->lastInsertId());
-
-            // execute the second query
-            if (!$query2->execute()) {
-                // rollback on error
-                $this->db->rollBack();
-                return false;
-            }
-
-            // if all is OK, commit the transaction
-            $this->db->commit();
-            return true;
-
-        } catch (Exception $e) {
-            // rollback on any error
-            $this->db->rollBack();
-            return $e->getMessage();
-        }
-    }
-
-
-    /**
      * Logs in a user by verifying credentials.
      *
      * @param string $username       The username of the user.
