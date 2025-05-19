8745c0598f0d8819dfbb2a0b8644138794f7edba
Yasen Pramatarov <yasen@lindeas.com>
2025-05-19 15:05:07 +0300

    Adds an example theme "modern"

---
+++ themes/modern/config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'Modern theme',
+    'description' => 'Example theme. A modern, clean theme for Jilo Web.',
+    'version' => '1.0.0',
+    'author' => 'Lindeas Inc.',
+    'screenshot' => 'screenshot.png',
+    'options' => [
+        // Theme-specific options can be defined here
+    ]
+];
