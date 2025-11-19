85ea44c1e396155317a74be76f70404ec2d64b1f
Yasen Pramatarov <yasen@lindeas.com>
2025-11-19 21:43:30 +0200

    Redesigns register button

--- plugins/register/bootstrap.php
+++ plugins/register/bootstrap.php
@@ -20,5 +20,7 @@ define('REGISTRATIONS_MAIN_MENU_POSITION', 30);
 register_hook('main_public_menu', function($ctx) {
     $section = defined('REGISTRATIONS_MAIN_MENU_SECTION') ? REGISTRATIONS_MAIN_MENU_SECTION : 'main';
     $position = defined('REGISTRATIONS_MAIN_MENU_POSITION') ? REGISTRATIONS_MAIN_MENU_POSITION : 100;
-    echo '<li><a href="?page=register">register</a></li>';
+    echo "                    <button class=\"btn modern-header-btn\" onclick=\"window.location.href='" . htmlspecialchars($app_root) . "?page=register'\">
+                        <i class=\"fas fa-user-edit me-2\"></i>Register
+                    </button>";
 });
