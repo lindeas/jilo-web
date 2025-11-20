785e9a84eb4a5902cd51d070d5e6185d908b9072
Yasen Pramatarov <yasen@lindeas.com>
2025-11-20 12:45:18 +0200

    Monthly dashboard stats redesign

--- app/templates/dashboard-monthly.php
+++ app/templates/dashboard-monthly.php
@@ -1,64 +1,65 @@
-
-                <div class="row">
-
-<?php if ($widget['collapsible'] === true) { ?>
-                    <a style="text-decoration: none;" data-toggle="collapse" href="#collapse<?= htmlspecialchars($widget['name']) ?>" role="button" aria-expanded="true" aria-controls="collapse<?= htmlspecialchars($widget['name']) ?>">
-                        <div class="card w-auto bg-light card-body"  style="flex-direction: row;"><?= $widget['title'] ?></div>
-<?php } else { ?>
-                    <div class="card w-auto bg-light border-light card-body"  style="flex-direction: row;"><?= $widget['title'] ?></div>
-<?php } ?>
-<?php if ($widget['filter'] === true) {
-    include '../app/templates/block-results-filter.php'; } ?>
-<?php if ($widget['collapsible'] === true) { ?>
-                    </a>
-<?php } ?>
-
-                </div>
-
-                <!-- widget "<?= htmlspecialchars($widget['name']) ?>" -->
-                <div class="collapse show" id="collapse<?= htmlspecialchars($widget['name']) ?>">
+                <section class="tm-widget-card tm-call-widget">
+                    <div class="tm-widget-header">
+                        <div>
+                            <p class="tm-widget-eyebrow">Conferences</p>
+                            <h3 class="tm-widget-title">
+                                <?= $widget['title'] ?>
+                            </h3>
 <?php if ($time_range_specified) { ?>
-                    <p class="m-3">time period:
-                        <strong>
-                            <?= $from_time == '0000-01-01' ? 'beginning' : date('d M Y', strtotime($from_time)) ?> - <?= $until_time == '9999-12-31' ? 'now' : date('d M Y', strtotime($until_time)) ?>
-                        </strong>
-                    </p>
+                            <p class="m-1 mb-0" style="font-size: 0.75rem;">time period:
+                                <strong>
+                                    <?= $from_time == '0000-01-01' ? 'beginning' : date('d M Y', strtotime($from_time)) ?> - <?= $until_time == '9999-12-31' ? 'now' : date('d M Y', strtotime($until_time)) ?>
+                                </strong>
+                            </p>
 <?php } ?>
-                    <div class="mb-5">
-<?php if ($widget['full'] === true) { ?>
-                        <table class="table table-results table-striped table-hover table-bordered">
-                            <thead class="thead-dark">
-                                <tr>
-                                    <th scope="col"></th>
-<?php     foreach ($widget['records'] as $record) { ?>
-                                    <th scope="col"><?= htmlspecialchars($record['table_headers']) ?></th>
-<?php     } ?>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>conferences</td>
-<?php     foreach ($widget['records'] as $record) { ?>
-                                    <td><?php if (!empty($record['conferences'])) { ?>
-                                        <a href="<?= htmlspecialchars($app_root) ?>?platform=<?= htmlspecialchars($platform_id) ?>&page=conferences&from_time=<?= htmlspecialchars($record['from_time']) ?>&until_time=<?= htmlspecialchars($record['until_time']) ?>"><?= htmlspecialchars($record['conferences']) ?></a> <?php } else { ?>
-                                        0<?php } ?>
-                                    </td>
-<?php     } ?>
-                                </tr>
-                                <tr>
-                                    <td>participants</td>
-<?php     foreach ($widget['records'] as $record) { ?>
-                                    <td><?php if (!empty($record['participants'])) { ?>
-                                        <a href="<?= htmlspecialchars($app_root) ?>?platform=<?= htmlspecialchars($platform_id) ?>&page=participants&from_time=<?= htmlspecialchars($record['from_time']) ?>&until_time=<?= htmlspecialchars($record['until_time']) ?>"><?= htmlspecialchars($record['participants']) ?></a> <?php } else { ?>
-                                        0<?php } ?>
-                                    </td>
-<?php     } ?>
-                                </tr>
-                            </tbody>
-                        </table>
-<?php } else { ?>
-                        <p class="m-3">No matching records found.</p>
-<?php } ?>
+                        </div>
+                        <div class="tm-widget-tools">
+<?php if ($widget['filter'] === true) { include '../app/templates/block-results-filter.php'; } ?>
+                        </div>
                     </div>
-                </div>
-                <!-- /widget "<?= htmlspecialchars($widget['name']) ?>" -->
+
+                    <!-- calls -->
+                    <div class="tm-widget-body">
+                            <div class="table-responsive">
+                                <table class="table tm-widget-table" style="font-size: 0.75rem;">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col"></th>
+<?php     foreach ($widget['records'] as $record) { ?>
+                                            <th scope="col"><?= htmlspecialchars($record['table_headers']) ?></th>
+<?php     } ?>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+<?php if (!empty($widget['records'])) { ?>
+                                        <tr>
+                                            <td>conferences</td>
+<?php     foreach ($widget['records'] as $record) { ?>
+                                            <td><?php if (!empty($record['conferences'])) { ?>
+                                                <a href="<?= htmlspecialchars($app_root) ?>?platform=<?= htmlspecialchars($platform_id) ?>&page=conferences&from_time=<?= htmlspecialchars($record['from_time']) ?>&until_time=<?= htmlspecialchars($record['until_time']) ?>"><?= htmlspecialchars($record['conferences']) ?></a> <?php } else { ?>
+                                                0<?php } ?>
+                                            </td>
+<?php     } ?>
+                                        </tr>
+                                        <tr>
+                                            <td>participants</td>
+<?php     foreach ($widget['records'] as $record) { ?>
+                                            <td><?php if (!empty($record['participants'])) { ?>
+                                                <a href="<?= htmlspecialchars($app_root) ?>?platform=<?= htmlspecialchars($platform_id) ?>&page=participants&from_time=<?= htmlspecialchars($record['from_time']) ?>&until_time=<?= htmlspecialchars($record['until_time']) ?>"><?= htmlspecialchars($record['participants']) ?></a> <?php } else { ?>
+                                                0<?php } ?>
+                                            </td>
+<?php     } ?>
+                                        </tr>
+<?php } else { ?>
+                                        <tr>
+                                            <td colspan="6">
+                                                <p class="tm-widget-empty">No matching records found.</p>
+                                            </td>
+                                        </tr>
+<?php } ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    <!-- /monthly conferences -->
+                </section>
