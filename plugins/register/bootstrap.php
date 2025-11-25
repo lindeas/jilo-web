014ef05d059f1fc24ad695f84b5a67a6280559fc
Yasen Pramatarov <yasen@lindeas.com>
2025-11-25 12:25:42 +0200

    Fix in register plugin

--- plugins/register/bootstrap.php
+++ plugins/register/bootstrap.php
@@ -20,7 +20,8 @@ define('REGISTRATIONS_MAIN_MENU_POSITION', 30);
 register_hook('main_public_menu', function($ctx) {
     $section = defined('REGISTRATIONS_MAIN_MENU_SECTION') ? REGISTRATIONS_MAIN_MENU_SECTION : 'main';
     $position = defined('REGISTRATIONS_MAIN_MENU_POSITION') ? REGISTRATIONS_MAIN_MENU_POSITION : 100;
-    echo "                    <button class=\"btn modern-header-btn\" onclick=\"window.location.href='" . htmlspecialchars($app_root) . "?page=register'\">
+    $appRoot = isset($ctx['app_root']) ? htmlspecialchars($ctx['app_root'], ENT_QUOTES, 'UTF-8') : '';
+    echo "                    <button class=\"btn modern-header-btn\" onclick=\"window.location.href='" . $appRoot . "?page=register'\">
                         <i class=\"fas fa-user-edit me-2\"></i>Register
                     </button>";
 });
